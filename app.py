c5c713e333e6b90c6a367d624c0b4da7a73cd323
DenisLevadny <otdelproden@gmail.com>
2023-05-11 23:37:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,9 @@ def notify():
          category = logs['event']['activity'][0]['category']
       except:
          print("category not defined")
-      
-         if logs['webhookId']==os.environ['ALCHEMY_KEY'] and category == 'token':
+         
+         webhook_id = logs['webhookId']
+         if (webhook_id == os.environ['ALCHEMY_KEY_GOERLY'] or webhook_id == os.environ['ALCHEMY_KEY_MUMBAI'] or webhook_id == os.environ['ALCHEMY_KEY_BSC']) and category == 'token':
            # get the network name
            network = logs['event']['network']
 
