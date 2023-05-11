bdb3a763f07adc23905b16357d45854b73460d4f
DenisLevadny <otdelproden@gmail.com>
2023-05-11 22:53:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,21 +29,39 @@ def notify():
       except:
          print("category not defined")
       
-      if logs['webhookId']==os.environ['ALCHEMY_KEY'] and category == 'token':
-        # extract the necessary information
-        txhash = from_address = "["+str(logs['event']['activity'][0]['hash'])+"](https://etherscan.io/tx/"+str(logs['event']['activity'][0]['hash'])+")"
-         
-        from_address = "["+str(logs['event']['activity'][0]['fromAddress'])+"](https://etherscan.io/address/"+str(logs['event']['activity'][0]['fromAddress'])+"#tokentxns)"
-        to_address = "["+str(logs['event']['activity'][0]['toAddress'])+"](https://etherscan.io/address/"+str(logs['event']['activity'][0]['toAddress'])+"#tokentxns)"
-        
-        token_symbol = logs['event']['activity'][0]['asset']
-        token_address = "["+str(logs['event']['activity'][0]['rawContract']['address'])+"](https://etherscan.io/address/"+str(logs['event']['activity'][0]['rawContract']['address'])+")"
-        
-        value = str(round(logs['event']['activity'][0]['value']))
+         if logs['webhookId']==os.environ['ALCHEMY_KEY'] and category == 'token':
+           # get the network name
+           network = logs['event']['network']
 
-        # create the text string
-        message = f'*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue: {value} *{token_symbol}* {token_address}'
-        bot.send_message(chat_id=user_chat_id, text=message, parse_mode='MarkdownV2')
+           # check the network and set the domain accordingly
+           if network == 'ETH_GOERLI':
+             domain = 'goerli.etherscan.io'
+           elif network == 'ETH_MAINNET':
+             domain = 'etherscan.io'
+           elif network == 'POLYGON_MAINNET':
+             domain = 'polygonscan.com'
+           elif network == 'BSC_MAINNET':
+             domain = 'bscscan.com'
+           elif network == 'MATIC_MUMBAI':
+             domain = 'mumbai.polygonscan.com'
+           else:
+             # unknown network, skip the processing
+             return Response(status=200)
+
+           # extract the necessary information
+           txhash = "["+str(logs['event']['activity'][0]['hash'])+"](https://"+domain+"/tx/"+str(logs['event']['activity'][0]['hash'])+")"
+           
+           from_address = "["+str(logs['event']['activity'][0]['fromAddress'])+"](https://"+domain+"/address/"+str(logs['event']['activity'][0]['fromAddress'])+"#tokentxns)"
+           to_address = "["+str(logs['event']['activity'][0]['toAddress'])+"](https://"+domain+"/address/"+str(logs['event']['activity'][0]['toAddress'])+"#tokentxns)"
+          
+           token_symbol = logs['event']['activity'][0]['asset']
+           token_address = "["+str(logs['event']['activity'][0]['rawContract']['address'])+"](https://"+domain+"/address/"+str(logs['event']['activity'][0]['rawContract']['address'])+")"
+          
+           value = str(round(logs['event']['activity'][0]['value']))
+
+           # create the text string
+           message = f'*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue: {value} *{token_symbol}* {token_address}'
+           bot.send_message(chat_id=user_chat_id, text=message, parse_mode='MarkdownV2')
       
   return Response(status=200)
 
