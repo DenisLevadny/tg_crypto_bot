a31293d2d3308d168bbca7c660cb1056785d5d18
DenisLevadny <otdelproden@gmail.com>
2023-05-14 00:49:47 +0300

    Delete ping.py

--- ping.py
+++
@@ -1,9 +0,0 @@
-import requests
-import time
-
-while True:
-    try:
-        requests.get('https://test-crypto-notify-2.onrender.com/notify') # вставьте код, который выполняется, когда сайт работает
-    except:
-        # вставьте код, который выполняется, когда сайт не работает
-    time.sleep(600) # 600 секунд = 10 минут
