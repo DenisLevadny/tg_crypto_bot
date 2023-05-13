f8c3170265f14cbf64648a7743618f362843b66c
DenisLevadny <otdelproden@gmail.com>
2023-05-13 18:18:26 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -3,7 +3,7 @@ import time
 
 while True:
     try:
-        requests.get('https://your-app.render.com') # вставьте код, который выполняется, когда сайт работает
+        requests.get('https://test-crypto-notify-2.onrender.com/notify/') # вставьте код, который выполняется, когда сайт работает
     except:
         # вставьте код, который выполняется, когда сайт не работает
     time.sleep(30) # 600 секунд = 10 минут
